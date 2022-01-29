57a5f16a1f8c5c48edf6bfb2ec2661cfa92d2b27
afer92 <11302604+afer92@users.noreply.github.com>
2022-01-29 17:42:24 +0100

    pycodestyle enhancements

--- lywsd03mmc/lywsd03mmc.py
+++ lywsd03mmc/lywsd03mmc.py
@@ -7,12 +7,14 @@ UUID_HISTORY = 'EBE0CCBC-7A0A-4B0C-8A1A-6FF2997DA3A6'  # Last idx 152          R
 UUID_FIRMWARE = '00002a26-0000-1000-8000-00805f9b34fb'   # handle: 0x0012 firmware revision
 UUID_HARDWARE = '00002a27-0000-1000-8000-00805f9b34fb'   # handle: 0x0014 hardware revision
 
-# Create a structure to store the data in, which includes battery data 
+
+# Create a structure to store the data in, which includes battery data
 class SensorDataBattery(collections.namedtuple('SensorDataBase', ['temperature', 'humidity', 'battery'])):
     __slots__ = ()
 
+
 class Lywsd03mmcClient(Lywsd02Client):
-    
+
     # Temperature units specific to LYWSD03MMC devices
     UNITS = {
         b'\x01': 'F',
@@ -33,20 +35,20 @@ class Lywsd03mmcClient(Lywsd02Client):
         voltage /= 1000
 
         # Estimate the battery percentage remaining
-        battery = min(int(round((voltage - 2.1),2) * 100), 100) # 3.1 or above --> 100% 2.1 --> 0 %
+        battery = min(int(round((voltage - 2.1), 2) * 100), 100)  # 3.1 or above --> 100% 2.1 --> 0 %
         self._data = SensorDataBattery(temperature=temperature, humidity=humidity, battery=battery)
 
     # Battery data comes along with the temperature and humidity data, so just get it from there
     @property
     def battery(self):
         return self.data.battery
-    
+
     def _get_history_data(self):
         # Get the time the device was first run
         self.start_time
-        
+
         # Work out the expected last record we'll be sent from the device.
-        # The current hour doesn't appear until the end of the hour, and the time is recorded as 
+        # The current hour doesn't appear until the end of the hour, and the time is recorded as
         # the end of hour time
         expected_end = datetime.now() - timedelta(hours=1)
 
@@ -77,11 +79,11 @@ class Lywsd03mmcClient(Lywsd02Client):
 
     # Getting history data is very slow, so output progress updates
     enable_history_progress = False
+
     def output_history_progress(self, ts, min_temp, max_temp):
         if not self.enable_history_progress:
             return
         print("{}: {} to {}".format(ts, min_temp, max_temp))
-
 
     # Locally cache the start time of the device.
     # This value won't change, and caching improves the performance getting the history data
@@ -92,23 +94,25 @@ class Lywsd03mmcClient(Lywsd02Client):
     @property
     def start_time(self):
         if not self._start_time:
-            start_time_delta = self.time[0] - datetime(1970,1,1) - timedelta(hours=self.tz_offset)
+            start_time_delta = self.time[0] - datetime(1970, 1, 1) - timedelta(hours=self.tz_offset)
             self._start_time = datetime.now() - start_time_delta
         return self._start_time
 
-
-    # Disable setting the time and timezone. 
+    # Disable setting the time and timezone.
     # LYWSD03MMCs don't have visible clocks
+
     @property
     def time(self):
         return super().time
+
     @time.setter
     def time(self, dt: datetime):
         return
 
     @property
-    def tz_offset(self): 
+    def tz_offset(self):
         return super().tz_offset
+
     @tz_offset.setter
     def tz_offset(self, tz_offset: int):
         return
