5eaa71f70c318373d49b45e45bc08afb9dad2a25
afer92 <11302604+afer92@users.noreply.github.com>
2022-02-20 11:52:23 +0100

    Add scan lywsd03mmc devices with custom firmware

--- lywsd03mmc/lywsd03mmc.py
+++ lywsd03mmc/lywsd03mmc.py
@@ -3,9 +3,22 @@ import struct
 import collections
 from datetime import datetime, timedelta
 
+import re
+from bluepy.btle import Scanner, DefaultDelegate
+import bluepy
+
 UUID_HISTORY = 'EBE0CCBC-7A0A-4B0C-8A1A-6FF2997DA3A6'  # Last idx 152          READ NOTIFY
 UUID_FIRMWARE = '00002a26-0000-1000-8000-00805f9b34fb'   # handle: 0x0012 firmware revision
 UUID_HARDWARE = '00002a27-0000-1000-8000-00805f9b34fb'   # handle: 0x0014 hardware revision
+
+
+def valid_miflora_mac(mac):
+    refilter = r"(A4:C1:38)|(A4:C1:38):[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}"
+    pat = re.compile(refilter)
+    """Check for valid mac adresses."""
+    if not pat.match(mac.upper()):
+        return False
+    return True
 
 
 # Create a structure to store the data in, which includes battery data
@@ -132,3 +145,158 @@ class Lywsd03mmcClient(Lywsd02Client):
             value = ch.read()
             firmware = ''.join(map(chr, value))
         return firmware
+
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if valid_miflora_mac(dev.addr) is False:
+            return
+        if isNewDev:
+            print("Discovered device", dev.addr)
+        elif isNewData:
+            print("Received new data from", dev.addr)
+
+
+class AtcMiThermometerDevice():
+
+    def _process_sensor_data(self, data):
+        datas = []
+        for i in range(0, 15):
+            datas.append(data[i*2:2+i*2])
+        self._mac = ':'.join(datas[2:8]).upper()
+        self._temp = int('0x' + ''.join(datas[8:10]), base=16)/10
+        self._hum = int('0x' + ''.join(datas[10:11]), base=16)
+        self._battery = int('0x' + ''.join(datas[11:12]), base=16)
+        self._volts = int('0x' + ''.join(datas[12:14]), base=16)/1000
+        self._skip = False
+        if datas[14] == '':
+            print('skip')
+            self._skip = True
+        self._rssi = None
+
+    def __init__(self, data):
+        self._process_sensor_data(data)
+
+    def __str__(self):
+        result = '{}: \t{}'.format('mac', self._mac)
+        result += '\n{}: \t{} °C'.format('temp', self._temp)
+        result += '\n{}: \t{} %'.format('hum', self._hum)
+        result += '\n{}: \t{} %'.format('batt', self._battery)
+        result += '\n{}: \t{} V'.format('volts', self._volts)
+        result += '\n{}: \t{} dB'.format('rssi', self._rssi)
+        result += '\n{}: \t{}'.format('skip', self._skip)
+        return result
+
+    @property
+    def mac(self):
+        return self._mac
+
+    @property
+    def temp(self):
+        return self._temp
+
+    @property
+    def hum(self):
+        return self._hum
+
+    @property
+    def battery(self):
+        return self._battery
+
+    @property
+    def volts(self):
+        return self._volts
+
+    @property
+    def skip(self):
+        return self._skip
+
+    @property
+    def rssi(self):
+        return self._rssi
+
+    @rssi.setter
+    def rssi(self, value):
+        self._rssi = value
+        return
+
+
+class AtcMiThermometerClient():
+
+    def __init__(self, scan_for=15.0, retry=3):
+        self._scan_for = scan_for
+        self._retry = retry
+        self._devices = []
+        self._thermometers = []
+
+    def get_datas(self):
+        devices = []
+        scanner = Scanner().withDelegate(ScanDelegate())
+        try:
+            devices = scanner.scan(self._scan_for)
+        except (bluepy.btle.BTLEManagementError,
+                bluepy.btle.BTLEDisconnectError) as err:
+            print(err)
+            print('Proceed...')
+            # exit()
+        self._devices = devices
+        self._process_datas()
+
+    def _process_datas(self):
+        for dev in self._devices:
+            if valid_miflora_mac(dev.addr) is False:
+                continue
+            strf = "Device {} ({}), RSSI={} dB"
+            # print(strf.format(dev.addr, dev.addrType, dev.rssi))
+            local_name = ''
+            value = ''
+            for (adtype, desc, val) in dev.getScanData():
+                print("  %s = %s" % (desc, val))
+                if desc == "16b Service Data":
+                    value = val
+                elif desc == "Complete Local Name":
+                    local_name = val
+
+            # print('{} : {}'.format(local_name, value))
+            if (local_name == '') or (value == ''):
+                continue
+            # print("  %s = %s" % (desc, value))
+
+            thermometer = AtcMiThermometerDevice(value)
+            thermometer.rssi = dev.rssi
+            print(thermometer)
+            self._thermometers.append(thermometer)
+
+    @property
+    def scan_for(self):
+        return self._scan_for
+
+    @scan_for.setter
+    def scan_for(self, value):
+        self._scan_for = value
+        return
+
+    @property
+    def retry(self):
+        return self._retry
+
+    @retry.setter
+    def retry(self, value):
+        self._retry = value
+        return
+
+    @property
+    def thermometers(self):
+        return self._thermometers
+
+
+def main():
+    atc = AtcMiThermometerClient()
+    atc.get_datas()
+
+
+if __name__ == '__main__':
+    main()
